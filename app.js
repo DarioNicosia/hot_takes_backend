017492e534524d5181f0e8ceb1ef03116facefa1
Dario Nicosia <darionicosia@hotmail.com>
2019-09-19 13:08:51 +0100

    installed mongoose, imported it on app.js and connected app to mongodb using connect method that return a promise

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
+//password mongodb: uQ2tb9ESYdyZ292x
+//connect MONGODB: mongodb+srv://dario:<password>@cluster0-kqqh5.mongodb.net/test?retryWrites=true&w=majority
+
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express()
+
+//connect to MONGODB - npm install --save mongoose
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://dario:uQ2tb9ESYdyZ292x@cluster0-kqqh5.mongodb.net/test?retryWrites=true&w=majority')
+.then(() => {
+  console.log('Successfully connected to MongoDB Atlas!');
+})
+.catch((error) => {
+  console.log('Unable to connect to MongoDB Atlas!');
+  console.error(error);
+});
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');

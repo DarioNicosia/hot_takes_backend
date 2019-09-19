5b7f134f5e999b80386447e5526c647a3a9a39ad
Dario Nicosia <darionicosia@hotmail.com>
2019-09-19 16:48:05 +0100

    Implemented signup function creating a new user. First imported user model to controllers/user.js file to create a new user. Then installed bcrypt package and import it in order to hash user password. In signup function used bcrypt.hash function asking it to salt the password 10 times.Then hash function return a promise which receive the produced hash.Then created a new User with req.body.email for the email data and the produced hash for password data. Then saved it to database.

--- controllers/user.js
+++ controllers/user.js
@@ -1,8 +1,29 @@
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
+
 exports.signup =(req,res,next)=>{
-
-
-
-}
+    bcrypt.hash(req.body.password, 10).then(
+        (hash)=>{
+            const user = new User ({
+                email:req.body.email,
+                password:hash
+            });
+           user.save().then(
+            ()=>{
+                req.status(201).json({
+                    message:'user added succesfully'
+                })
+            }   
+           ).catch(
+               (error)=>{
+                   req.status(500).json({
+                       error:error
+                   });
+               }
+           ); 
+        }
+    );
+};
 
 exports.login =(req,re,next)=>{
 

cf2023345481e2ca3c8233bba6ecf0f257a79129
Dario Nicosia <darionicosia@hotmail.com>
2019-09-22 18:41:42 +0100

    Implemented authentication middleware In middleware folder created a auth.js file to contains the authentication middleware. Imported jwt on it. In the middleware function created a try/catch block. In the catch block sent back a 401 status and created a new Error sending back a message of invalid request. Created a const using the req.headers.authorization to extract the token. Then split it and take the second element of the array in order to take only the token and not also the bearer keyword. Then created a const called decodedToken  and used jwt package with verify function which takes two arguments: first argument is the token needs to be verified and the second  is the random string used to encode it. Then extract the userId from the decoded token. Then checked if the userId of the request body is the same or not of userId of decoded token Then imported in routes/sauce.js and added it on each route

--- routes/sauce.js
+++ routes/sauce.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router()
 const multer = require('../middleware/multer-config');
+const auth = require('../middleware/auth');
 
 const sauceCtrl = require('../controllers/sauce');
 
-router.post('/', multer, sauceCtrl.createSauce);
-router.get('/',sauceCtrl.getAllSauces);
-router.get('/:id',sauceCtrl.getOneSauce);
-router.put('/:id',multer,sauceCtrl.modifySauce);
-router.delete('/:id',sauceCtrl.deleteSauce);
+router.post('/',auth, multer, sauceCtrl.createSauce);
+router.get('/',auth,sauceCtrl.getAllSauces);
+router.get('/:id',auth,sauceCtrl.getOneSauce);
+router.put('/:id',auth,multer,sauceCtrl.modifySauce);
+router.delete('/:id',auth,sauceCtrl.deleteSauce);
 
 module.exports = router;
